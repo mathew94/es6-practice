b00f29ddceb1345fe5852b514a98ffaf31a1ca7d
mathew94 <mathewrodrick94@hotmail.com>
2020-08-07 19:46:49 +0600

    done

---
+++ default.js
@@ -0,0 +1,6 @@
+function add(num1, num2 = 0){
+    return num1 + num2;
+}
+// jodi value na dia dei tahole uporer 0 nia nibe
+const total = add(15, 1);
+console.log(total); 
---
+++ letConst.js
@@ -0,0 +1,7 @@
+const hubby = "Elias Kanchon";
+console.log(hubby);
+
+const numbers = [15, 45];
+numbers[1] = 66;
+numbers.push(78); 
+console.log(numbers);
